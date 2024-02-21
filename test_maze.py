c676917f0481a4359e1276a2e0f096085c381d5e
MrDataStructures <108447408+MrDataStructures@users.noreply.github.com>
2024-02-21 17:35:12 -0500

    Update test_maze.py

--- test_maze.py
+++ test_maze.py
@@ -1,26 +1,32 @@
-from gen_maze import generate_maze, visualize_maze, visualize_maze_with_path
+from gen_maze import visualize_maze, visualize_maze_with_path, load_mazes
 from a_star import a_star
 
 def main():
-    maze_size = 10 #change this value and run this file to test out difference sized mazes
-    maze = generate_maze(maze_size)
-    maze[0][0].blocked = False
-    maze[maze_size - 1][maze_size - 1].blocked = False
-    visualize_maze(maze)
+    maze_size = 101
+    folder_path = "proj1/mazes" 
+    maze_index_to_test = 0  # Change this index to test a different maze
 
-    start = (maze_size - 1, maze_size - 1) #bottom right of the maze
-    goal = (0, 0) # top left of the maze
+    loaded_mazes = load_mazes(folder_path)
 
-    path = a_star(maze, start, goal)
+    if 0 <= maze_index_to_test < len(loaded_mazes):
+        maze = loaded_mazes[maze_index_to_test]
 
-    if path:
-        print("\nPath Found:")
-        for step in path:
-            print(step)
-        
-        visualize_maze_with_path(maze, path)
+        start = (maze_size - 1, maze_size - 1)
+        goal = (0, 0)
+
+        visualize_maze(maze)
+        path = a_star(maze, start, goal)
+
+        if path:
+            print("\nPath Found:")
+            for step in path:
+                print(step)
+
+            visualize_maze_with_path(maze, path)
+        else:
+            print("\nNo path found.")
     else:
-        print("\nNo path found.")
+        print(f"Invalid maze index {maze_index_to_test}. Please choose a valid index.")
 
 if __name__ == "__main__":
     main()
